95f83610c5c0961690bf0e3857447601da87292f
Nicole Lopez <nmlopez21@gmail.com>
2015-03-15 21:24:11 -0500

    able to manipulate certain cells

--- app.js
+++ app.js
@@ -39,20 +39,12 @@ for each cell that's dead...
 // createBoard(boardHeight, boardWidth);
 
 var lastClicked;
-var grid = clickableGrid(30,30,function(el,row,col,i){
-    console.log("You clicked on element:",el);
-    console.log("You clicked on row:",row);
-    console.log("You clicked on col:",col);
-    console.log("You clicked on item #:",i);
-
-    el.className='clicked';
-    if (lastClicked) lastClicked.className='';
-    lastClicked = el;
+var grid = makeGrid(30,30,function(el,row,col,i){
 });
 
 document.body.appendChild(grid);
-     
-function clickableGrid( rows, cols, callback ){
+
+function makeGrid( rows, cols, callback ){
     var i=0;
     var grid = document.createElement('table');
     grid.className = 'grid';
@@ -60,12 +52,11 @@ function clickableGrid( rows, cols, callback ){
         var tr = grid.appendChild(document.createElement('tr'));
         for (var c=0;c<cols;++c){
             var cell = tr.appendChild(document.createElement('td'));
-            //cell.innerHTML = ++i;
-            cell.addEventListener('click',(function(el,r,c,i){
-                return function(){
-                    callback(el,r,c,i);
-                }
-            })(cell,r,c,i),false);
+            cell.id = ++i;
+            if (cell.id % 5 === 0) {
+                var alive = true;
+                //console.log(cell.id);
+            }
         }
     }
     return grid;

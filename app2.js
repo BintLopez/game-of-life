bf14c7583545bd2ece10086d123c5c3c178d7204
Nicole Lopez <nmlopez21@gmail.com>
2015-04-30 19:19:05 -0500

    trying to fix findNeighbors function

--- app2.js
+++ app2.js
@@ -36,81 +36,87 @@ function cellDisplay() {
 //finds the neighbors for each cell given its coordinates
 //should refactor this to take a cells y and x and return array of neighbors -- purpose of this is to take neighbors array out of the cell object
 function findNeighbors(y, x) {
+	//console.log("kittens inside the findNeighbors function!")
 
 	//neighbors for cells at top edge of grid
 	if (y === 0 && 0 < x && x < (cols-1)) {
-		neighbors = [ cells[y][x-1], cells[y][x+1], cells[y+1][x-1], cells[y+1][x], cells[y+1][x+1] ];
+		var neighbors = [ cells[y][x-1], cells[y][x+1], cells[y+1][x-1], cells[y+1][x], cells[y+1][x+1] ];
 	}
 	//neighbors for cells at bottom edge of grid
 	else if (y === (rows-1) && 0 < x && x < (cols-1)) {
-		neighbors = [ cells[y-1][x-1], cells[y-1][x], cells[y-1][x+1], cells[y][x-1], cells[y][x+1] ];				
+		var neighbors = [ cells[y-1][x-1], cells[y-1][x], cells[y-1][x+1], cells[y][x-1], cells[y][x+1] ];				
 	}
 
 	//neighbors for cells on left edge of grid
 	else if (0 < y && y < (rows-1) && x === 0) {
-		neighbors = [ cells[y-1][x], cells[y-1][x+1], cells[y][x+1], cells[y+1][x], cells[y+1][x+1] ];
+		var neighbors = [ cells[y-1][x], cells[y-1][x+1], cells[y][x+1], cells[y+1][x], cells[y+1][x+1] ];
 	}
 			
 	//neighbors for cells on right edge of grid
 	else if (0 < y && y < (rows-1) && x === (cols-1)) {
-		neighbors = [ cells[y-1][x], cells[y-1][x-1], cells[y][x-1], cells[y+1][x], cells[y+1][x-1] ];
+		var neighbors = [ cells[y-1][x], cells[y-1][x-1], cells[y][x-1], cells[y+1][x], cells[y+1][x-1] ];
 	}
 
 	//neighbors for upper left corner cell
 	else if (y === 0 && x === 0) {
-		neighbors = [ cells[y][x+1], cells[y+1][x+1], cells[y+1][x] ];
+		var neighbors = [ cells[y][x+1], cells[y+1][x+1], cells[y+1][x] ];
 	}
 
 	//neighbors for upper right corner cell
 	else if (y === 0 && x === (cols-1)) {
-		neighbors = [ cells[y][x-1], cells[y+1][x-1], cells[y+1][x] ];
+		var neighbors = [ cells[y][x-1], cells[y+1][x-1], cells[y+1][x] ];
 	}
 
 	//neighbors for lower left corner cell
 	else if (y === (rows-1) && x === 0) {
-		neighbors = [ cells[y][x+1], cells[y-1][x+1], cells[y-1][x] ];
+		var neighbors = [ cells[y][x+1], cells[y-1][x+1], cells[y-1][x] ];
 	}
 
 	//neighbors for lower right corner cell
 	else if (y === (rows-1) && x === (cols-1)) {
-		neighbors = [ cells[y][x-1], cells[y-1][x-1], cells[y-1][x] ];
+		var neighbors = [ cells[y][x-1], cells[y-1][x-1], cells[y-1][x] ];
 	}
 			
 	//neighbors for all middle cells
 	else {
-		neighbors = [ cells[y-1][x-1], cells[y-1][x], cells[y-1][x+1], cells[y][x-1], cells[y][x+1], cells[y+1][x-1], cells[y+1][x], cells[y+1][x+1] ];
+		var neighbors = [ cells[y-1][x-1], cells[y-1][x], cells[y-1][x+1], cells[y][x-1], cells[y][x+1], cells[y+1][x-1], cells[y+1][x], cells[y+1][x+1] ];
 	}
-	console.log(neighbors);
+	//console.log(neighbors);
 	return neighbors;
 }
 
 //function that takes a cell obj & returns the number of alive neighbors
-function numAliveNeighbors(y, x) {
+//CHANGE THIS... PHASE OUT CELLS.NEIGHBORS 
+function numAliveNeighbors(neighbors, y, x) {
+	console.log('in the numAliveNeighbors function');
 	i = 0;
 	//console.log(cell);
 	var neighbors = cells[y][x].neighbors;
 	//console.log(neighbors);
-	for (n in neighbors) {
-		if (neighbors[n].isAlive === true) {
+	for (n=0; n < neighbors.length; n++) {
+		if (n.isAlive === true) {
 			i += 1;
 		}
 	}
 	//console.log(numAlive);
 	cells[y][x].numNeighbors = i;
+	console.log(cells[y][x].numNeighbors);
 }
 
 //given cell's coordinates checks if cell is alive & adds class alive
 function aliveCheck(y, x) {
+	//console.log("kittens inside the aliveCheck function!")
 	if (cells[y][x].isAlive === true) {
 		$('#cell_'+ y +'_' + x).addClass('alive');
 	}
 	else {
-		$('#cell_'+ y +'_' + x).addClass('dead');
+		//$('#cell_'+ y +'_' + x).addClass('dead');
 	}
 }
 
 //sets up which cells start out alive given the value numAliveStart -- this variable is defined as half the amount of cells in the grid
 function aliveInit(numAliveStart) {
+	console.log("kittens inside the aliveInit function!")
 	for (var i = 0; i < numAliveStart; i++) {
 		y = Math.floor(Math.random() * (rows - 0) + 0);
 		x = Math.floor(Math.random() * (cols - 0) + 0);
@@ -123,6 +129,7 @@ function aliveInit(numAliveStart) {
 
 //this takes a cell's coordinates & # of neighbors & determines whether the cell lives, dies, or regenerates for the next round 
 function generation(y, x) {
+	console.log("kittens inside the generation function!")
     if (cells[y][x].isAlive === true) {
     	// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
 	    if (cells[y][x].numNeighbors < 2) {
@@ -154,6 +161,34 @@ function autoUpdate() {
 
 }
 
+function play() {
+	//WHY DOES LINE 160 NOT WORK HERE?
+	//console.log('click');
+	for (y in cells) {
+		for (x in cells[y]) {
+			console.log('kittens in for loop');
+			findNeighbors(y, x);
+			numAliveNeighbors(neighbors, y, x);
+			generation(y, x);
+
+		}
+	}
+}
+
+function testInit() {
+	//TESTING GRID
+	cells[0][0].isAlive = true;
+	cells[0][1].isAlive = true;
+	cells[2][2].isAlive = true;
+	cells[3][0].isAlive = true;
+	cells[4][1].isAlive = true;
+	cells[4][3].isAlive = true;
+	cells[5][2].isAlive = true;
+	cells[5][3].isAlive = true;
+	cells[0][0].isAlive = true;
+}
+
+
 //FYI -- syntax declaring empty array (single instead of double) could be source of bugs in future
 var cells = [];
 var cols = 4;
@@ -168,36 +203,49 @@ cellDisplay();
 
 //initiate which cells start out alive
 //aliveInit(numAliveStart);
+testInit();
 
 
-cells[0][0].isAlive = true;
-cells[0][1].isAlive = true;
-cells[2][2].isAlive = true;
-cells[3][0].isAlive = true;
-cells[4][1].isAlive = true;
-cells[4][3].isAlive = true;
-cells[5][2].isAlive = true;
-cells[5][3].isAlive = true;
+$('#playBtn').click(play);
 
-for (y in cells) {
-	for (x in cells[y]) {
+
+//findNeighbors(1,3);
+
+for (var y = 0; y < cells.length; y++) {
+	for (var x = 0; x < cells[y].length; x++) {
+		//add alive class display
 		aliveCheck(y, x);
+		//calculates & returns array of neighboring cells
 	}
 }
 
-for (y in cells) {
-	for (x in cells[y]) {
+// logged_cells = {cells: cells}
+// console.log(logged_cells);
+// for (y in cells) {
+// 	console.log("y is "+y);
+// 	for (x in cells[y]) {
+// 		//console.log("2nd for loop")
+// 		console.log('x is '+x)
+// 		findNeighbors(y, x);
+// 		// numAliveNeighbors(y, x);
+// 		// console.log(neighbors);
+// 	}
+// }
 
+for (var y = 0; y < cells.length; y++) {
+	//console.log(cells[y]);
+	for (var x = 0; x < cells[y].length; x++) {
+		//console.log(cells[y][x]);
+		findNeighbors(y,x);
+		// console.log(neighbors);
+		// numAliveNeighbors(y,x,neighbors);
 	}
-}
-
+};
 
 // cells[4][3].isAlive = true;
 // aliveCheck(4, 3);
-// console.log($grid);
 
-// numAliveNeighbors(cells[2][3]);
-// console.log(cells[2][3].numNeighbors);
+// numAliveNeighbors(y, x);
 
 //generation();
 
@@ -210,13 +258,6 @@ for (y in cells) {
 // 	cells[4][3].isAlive = true;
 // }
 
-$('#playBtn').click(function() {
-	for (y in cells) {
-		for (x in cells[y]) {
-			generation(y, x);
-		}
-	}
-});
 
 	setInterval(function() {
 		autoUpdate;
